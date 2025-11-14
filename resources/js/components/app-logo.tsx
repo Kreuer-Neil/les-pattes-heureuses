3bfdca7091f4fbe799b15ea39413bce461efc540
Kreuer-Neil <neil.kreuer@student.hepl.be>
2025-11-14 14:17:57 +0100

    Added name to app-logo.tsx

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,6 +1,7 @@
 import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
+    const appname = import.meta.env.VITE_APP_NAME;
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
@@ -8,7 +9,7 @@ export default function AppLogo() {
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    { appname }
                 </span>
             </div>
         </>
