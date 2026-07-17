78ea8a55fe2b6f7653d5f6ff83c32945a6a062d0
Kreuer-Neil <neil.kreuer@student.hepl.be>
2026-07-17 10:52:34 +0200

    Fixed translation implementation

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,10 +1,13 @@
+// noinspection BadExpressionStatementJS
+
 import '../css/app.css';
 
+import i18n from '@/utils/i18n';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+import { StrictMode } from 'react';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,6 +19,8 @@ createInertiaApp({
             import.meta.glob('./pages/**/*.tsx'),
         ),
     setup({ el, App, props }) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        i18n;
         const root = createRoot(el);
 
         root.render(
