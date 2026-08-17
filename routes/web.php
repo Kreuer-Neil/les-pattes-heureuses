17a92ba3cbc39ac630d857e6ce5d97b098497faf
Kreuer-Neil <neil.kreuer@student.hepl.be>
2026-08-17 16:51:46 +0200

    Removed subdomain for Laravel Cloud

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Middleware\EnsurePasswordHasBeenChanged;
 use Illuminate\Support\Facades\Route;
 
+
 // Laravel recommends putting subdomain routes before root routes, to not let URIs get overwritten
 // Just in case.
-Route::domain('admin.'.'les-pattes-heureuses.test')->group(function () {
+
+Route::domain(parse_url(config('app.url'), PHP_URL_HOST).'/admin')->group(function () {
+//* Removing subdomain uses for Laravel Cloud compatibility
+//Route::domain('admin.'.parse_url(config('app.url'), PHP_URL_HOST))->group(function () {
+
     Route::middleware(['guest'])->group(function () {
         //        Route::get('/login', function () {
         //            return Inertia::render('auth/login', [
